fdb11a4ffaa1dfe633c22ade2b0de70ad9251323
Gamaliel Adun <adungamaliel@gmail.com>
2024-07-29 20:42:03 +0100

    add styling to input and add button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import './style.css';
-import TodoItem from './todo';
-import Project from './project';
-import { createNewProject, createNewTodo } from './utils';
+// import TodoItem from './todo';
+// import Project from './project';
+// import { createNewProject, createNewTodo } from './utils';
 
 const container = document.getElementById('content');
+
+const addTaskDiv = document.createElement('div');
+addTaskDiv.id = "add-task-container";
+
+const taskInput = document.createElement('input');
+taskInput.id = "task-input";
+taskInput.placeholder = "New task...";
+
+const addTaskBtn = document.createElement('button');
+addTaskBtn.id = "add-task-btn";
+addTaskBtn.textContent = "ADD";
+
+addTaskDiv.appendChild(taskInput);
+addTaskDiv.appendChild(addTaskBtn);
+
+
+
+
+container.appendChild(addTaskDiv);
