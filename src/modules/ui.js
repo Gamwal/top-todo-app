8112397e9efbc7010e11c7ca7c9e114089a1ee7c
Gamaliel Adun <adungamaliel@gmail.com>
2024-08-31 09:44:52 +0100

    remove dialog from ui module

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -67,69 +67,6 @@ function createProjectsDiv() {
   return projectsDiv;
 }
 
-function createNewTaskDialog() {
-  const popupDialog = document.createElement("div");
-  popupDialog.id = "popup-dialog";
-
-  const dialog = document.createElement("div");
-  dialog.id = "dialog";
-
-  const dialogTitle = document.createElement("div");
-  dialogTitle.textContent = "New Task";
-
-  const form = document.createElement("form");
-  form.id = "task-form";
-
-  const dialogBody = document.createElement("div");
-  dialogBody.id = "dialog-body";
-
-  const taskProject = document.createElement("select");
-
-  const taskTitle = document.createElement("input");
-  taskTitle.placeholder = "Title";
-  taskTitle.name = "title";
-
-  const taskDetails = document.createElement("input");
-  taskDetails.placeholder = "Notes";
-  taskDetails.name = "details";
-
-  const taskDueDate = document.createElement("input");
-  taskDueDate.type = "date";
-  taskDueDate.name = "dueDate";
-
-  const dialogControl = document.createElement("div");
-
-  const cancelButton = document.createElement("button");
-  cancelButton.classList.add("task-control-button");
-  cancelButton.id = "cancel-button";
-  cancelButton.type = "button";
-  cancelButton.textContent = "Cancel";
-
-  const doneButton = document.createElement("button");
-  doneButton.classList.add("task-control-button");
-  doneButton.id = "done-button";
-  doneButton.type = "submit";
-  doneButton.textContent = "Done";
-
-  dialogControl.appendChild(cancelButton);
-  dialogControl.appendChild(doneButton);
-
-  dialogBody.appendChild(taskProject);
-  dialogBody.appendChild(taskTitle);
-  dialogBody.appendChild(taskDetails);
-  dialogBody.appendChild(taskDueDate);
-
-  form.appendChild(dialogBody);
-  form.appendChild(dialogControl);
-
-  dialog.appendChild(dialogTitle);
-  dialog.appendChild(form);
-
-  popupDialog.appendChild(dialog);
-
-  return popupDialog;
-}
-
 function highlightTab(button) {
   button.classList.add("active-tab");
 }
@@ -141,4 +78,4 @@ function resetTab() {
   });
 }
 
-export { displayUI, createNewTaskDialog, highlightTab, resetTab };
+export { displayUI, highlightTab, resetTab };
